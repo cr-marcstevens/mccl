d587075fa40e02972328777f5d7c2c38333ce2d1
Wessel van Woerden <wesselvanwoerden@gmail.com>
2021-11-23 14:20:02 +0100

    test_enumerate

---
+++ tests/test_enumerate.cpp
@@ -0,0 +1,60 @@
+#include <mccl/config/config.hpp>
+
+#include <mccl/tools/enumerate.hpp>
+#include <mccl/tools/utils.hpp>
+
+#include "test_utils.hpp"
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <utility>
+
+using namespace mccl;
+
+int main(int, char**)
+{
+    int status = 0;
+
+    size_t k = 20; // number of columns
+
+
+    std::vector<uint64_t> firstwords(k, 0);
+    for(size_t i=0; i < k; i++)
+      firstwords[i] |= ((uint64_t) 1) << i;
+
+
+    enumerate_t<uint32_t> enumerate;
+
+    size_t p_up_to = 4;
+    for(size_t p = 1; p <= p_up_to; p++) {
+      std::set<uint64_t> vals;
+      std::vector<size_t> weights(p+1, 0);
+      enumerate.enumerate_val(firstwords.data(), firstwords.data()+k, p, 
+        [&vals, &weights, &status, p](uint64_t val)
+        {
+            size_t w = (size_t)__builtin_popcountll(val);
+            status |= (w > p);
+            vals.insert(val);
+            if(w<=p)
+              weights[w]++;
+            return true;
+        });
+
+      bigint_t expected_sums = 0;
+      for(size_t pp = 1; pp <= p; pp++) {
+        auto weight_pp = detail::binomial<bigint_t>(k,pp);
+        expected_sums += weight_pp;
+        status |= (weights[pp]!=weight_pp);
+      }
+
+      status |= (expected_sums!=vals.size());
+    }
+
+    if (status == 0)
+    {
+        LOG_CERR("All tests passed.");
+        return 0;
+    }
+    return -1;
+}
