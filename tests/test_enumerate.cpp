278f3d577d71ec0389bb3d1f35c45f860b38a305
Wessel van Woerden <wesselvanwoerden@gmail.com>
2021-11-23 15:21:50 +0100

    test enumerate

--- tests/test_enumerate.cpp
+++ tests/test_enumerate.cpp
@@ -27,6 +27,8 @@ int main(int, char**)
     enumerate_t<uint32_t> enumerate;
 
     size_t p_up_to = 4;
+
+    // test enumerate_val
     for(size_t p = 1; p <= p_up_to; p++) {
       std::set<uint64_t> vals;
       std::vector<size_t> weights(p+1, 0);
@@ -38,6 +40,48 @@ int main(int, char**)
             vals.insert(val);
             if(w<=p)
               weights[w]++;
+            return true;
+        });
+
+      bigint_t expected_sums = 0;
+      for(size_t pp = 1; pp <= p; pp++) {
+        auto weight_pp = detail::binomial<bigint_t>(k,pp);
+        expected_sums += weight_pp;
+        status |= (weights[pp]!=weight_pp);
+      }
+
+      status |= (expected_sums!=vals.size());
+    }
+
+    // test enumerate with indices
+    for(size_t p = 1; p <= p_up_to; p++) {
+      std::set<uint64_t> vals;
+      std::vector<size_t> weights(p+1, 0);
+      enumerate.enumerate(firstwords.data(), firstwords.data()+k, p, 
+        [&vals, &weights, &status, p](uint32_t* begin, uint32_t* end, uint64_t val)
+        {
+            size_t w = (size_t)__builtin_popcountll(val);
+            status |= (w > p);
+            vals.insert(val);
+            if(w<=p)
+              weights[w]++;
+
+            // indices check
+            size_t indices = end-begin;
+            if(indices != w) {
+              status |= 1;
+              std::cerr << "Incorrect number of indices: " << indices << " instead of " << w << std::endl;
+            }
+            std::set<uint32_t> indices_set;
+            for(size_t i=0; i<indices; i++) {
+              uint32_t ind = *(begin+i);
+              indices_set.insert(ind);
+              if(!(val&(((uint64_t) 1)<<ind))) {
+                status |= 1;
+              }
+            }
+            status |= (indices_set.size()!=indices);
+
             return true;
         });
 
